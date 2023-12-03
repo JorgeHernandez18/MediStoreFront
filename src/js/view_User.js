42ae10f9317a93a7ec86bd37e063d08b37be2c93
lesg1151860 <luiseduardosg@ufps.edu.co>
2023-12-02 21:54:11 -0500

    Consumo del .json para mostrar los datos en la tabla

---
+++ src/js/view_User.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    
+    // Obtener los datos JSON
+    $.getJSON("https://164.92.95.34:8095/usuario/api/usuarios", function(data) {
+      // Rellenar la tabla
+      $.each(data, function(i, item) {
+        var fila = $("<tr>");
+        fila.append($("<td>" + item.rol + "</td>"));
+        fila.append($("<td>" + item.nombre + "</td>"));
+        fila.append($("<td>" + item.apellido + "</td>"));
+        fila.append($("<td>" + item.telefono + "</td>"));
+
+        $("#datos").append(fila);
+      });
+
+      
+
+      
+    });
+  });
+
